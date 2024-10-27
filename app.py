825a80d9f342b0aeb2a41fb9e5ffad397a85417a
Adhitya S <adhityavalli@gmail.com>
2024-10-28 01:18:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,24 @@
 import os
 import json
 from PIL import Image
-
 import numpy as np
 import tensorflow as tf
 import streamlit as st
 
-
+# Set the working directory to the directory of the script
 working_dir = os.path.dirname(os.path.abspath(__file__))
-# Set the model path
-model_path = '/content/drive/MyDrive/plant-disease/plant-disease.h5'
+
+# Set the model path relative to the script
+model_path = os.path.join(working_dir, 'plant-disease.h5')
 
 # Load the model
 model = tf.keras.models.load_model(model_path)
 
-# loading the class names
-class_indices = json.load(open(f"{working_dir}/class_indices.json"))
+# Load the class names
+with open(os.path.join(working_dir, 'class_indices.json')) as f:
+    class_indices = json.load(f)
 
-
-# Function to Load and Preprocess the Image using Pillow
+# Function to load and preprocess the image using Pillow
 def load_and_preprocess_image(image_path, target_size=(224, 224)):
     # Load the image
     img = Image.open(image_path)
@@ -32,15 +32,13 @@ def load_and_preprocess_image(image_path, target_size=(224, 224)):
     img_array = img_array.astype('float32') / 255.
     return img_array
 
-
-# Function to Predict the Class of an Image
+# Function to predict the class of an image
 def predict_image_class(model, image_path, class_indices):
     preprocessed_img = load_and_preprocess_image(image_path)
     predictions = model.predict(preprocessed_img)
     predicted_class_index = np.argmax(predictions, axis=1)[0]
     predicted_class_name = class_indices[str(predicted_class_index)]
     return predicted_class_name
-
 
 # Streamlit App
 st.title('Plant Health Monitoring System')
